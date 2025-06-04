7343d2d8bc38db573c63e36526372d6be8a32c0e
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2025-06-04 17:56:18 +0300

    add root layput

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col ">
+        ROOT
+        <main className="wrapper flex-1">
+            {children}
+        </main>
+    </div>
+    
+  );
+}
