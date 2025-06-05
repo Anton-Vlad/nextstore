5a55141f8ff7d2520a41810e5ad56c2a8f9b9486
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2025-06-05 13:30:41 +0300

    NOtFound & Loading pages - done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
+
   return <>
   <div className="wrapper">
     <div className="text-3xl font-bold underline"> 
       Latest Products
     </div>
 
-    <Button>CLick</Button>
   </div>
     
   </>;
